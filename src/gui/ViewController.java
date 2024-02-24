7019dd6ae42b1cd7274a4b2e82760de5fb00b3d8
Carlos Aruh <carlos.aruh@gmail.com>
2024-02-24 15:03:59 -0300

    Mostrando Alert

--- src/gui/ViewController.java
+++ src/gui/ViewController.java
@@ -1,6 +1,8 @@
 package gui;
 
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 
 public class ViewController {
@@ -9,6 +11,6 @@ public class ViewController {
 	private Button btTest;
 
 	public void onBtTestAction() {
-		System.out.println("Click");
+		Alerts.showAlert("Alert title", null, "Hello", AlertType.ERROR);
 	}
 }
